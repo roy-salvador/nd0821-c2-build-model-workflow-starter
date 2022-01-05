1432744e8d8987aadff4c013f8b31102dea18d7b
roy-salvador <roy.salvador@gmail.com>
2022-01-05 13:16:30 +0800

    Add code for Initial Training

--- main.py
+++ main.py
@@ -61,8 +61,8 @@ def go(config: DictConfig):
                     "output_artifact": "clean_sample.csv",
                     "output_type": "clean_sample",
                     "output_description": "Data with outliers and null values removed",
-                    "min_price": config['etl']['min_price'],
-                    "max_price": config['etl']['max_price']
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
                },
             )
 
@@ -78,8 +78,8 @@ def go(config: DictConfig):
                     "csv": "clean_sample.csv:latest",
                     "ref": "clean_sample.csv:reference",
                     "kl_threshold": config["data_check"]["kl_threshold"],
-                    "min_price": config['etl']['min_price'],
-                    "max_price": config['etl']['max_price']
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
                },
             )
 
@@ -88,7 +88,18 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            # Split data into train, validation and test sets
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+               },
+            )
+
 
         if "train_random_forest" in active_steps:
 
@@ -103,16 +114,36 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config" : rf_config,
+                    "max_tfidf_features" : config["modeling"]["max_tfidf_features"],
+                    "output_artifact" : "random_forest_export"
+               },
+            )
 
-            pass
 
         if "test_regression_model" in active_steps:
 
             ##################
             # Implement here #
             ##################
+            # Evaluate on test set
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+               },
+            )
 
-            pass
 
 
 if __name__ == "__main__":
