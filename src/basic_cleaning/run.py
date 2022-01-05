e5db21048879e64f399e1ab528625123d6721b3c
roy-salvador <roy.salvador@gmail.com>
2022-01-05 17:52:05 +0800

    Filter data points outside NYC

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,9 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+    # Filter out data points outside NYC
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     # Save the dataframe locally
     df.to_csv("clean_sample.csv", index=False)
 
